0a51064f7d63a9c260447d96450ca50b8ca73095
Alan Alves <Andre.AlanAlves@br.nestle.com>
2021-09-27 09:14:19 -0300

    Generalizando o gerador de QRCODE

--- gen.py
+++ gen.py
@@ -1,18 +1,31 @@
-from re import I
+from re import I, T
+from numpy import iinfo
 import qrcode
 import pandas as pd
-from PIL import Image, ImageFont, ImageDraw
+from PIL import ImageFont, ImageDraw
 import os
 
-data = {}
-path = os.path.abspath("dados2.xlsx")
-df = pd.read_excel(path)
+documento = ""
+cabecalho = ""
 
-pessoas = df['Nº pess.'].values
-qr = qrcode.QRCode()
+# Funcão para checar as informações.
 
+
+def verificaDados():
+    documento = input("Nome da planilha com extensão:")
+    cabecalho = input("Cabecalho da planilha a ser usado:")
+    try:
+        path = os.path.abspath(documento)
+        df = pd.read_excel(path)
+        dados = df[cabecalho].values
+        print(dados)
+        return dados
+    except:
+        verificaDados()
 
 # Criar Diretório.
+
+
 def criaDiretorio():
     diretorio = "Exportado"
     try:
@@ -25,14 +38,16 @@ def criaDiretorio():
 
 
 caminho = criaDiretorio()
+dadosValidados = verificaDados()
+qr = qrcode.QRCode()
 
-for x in pessoas:
+for x in dadosValidados:
     qr = qrcode.QRCode(
-        version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=60, border=3)
+        version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=60, border=3)
     qr.add_data(x)
     qr.make(fit=True)
     img = qr.make_image(fill_color="black", back_color="white")
-    fonte = ImageFont.truetype("fonts/verdanab.ttf", 10)
+    fonte = ImageFont.truetype("fonts/verdanab.ttf", 60)
     img_edit = ImageDraw.Draw(img)
-    img_edit.text((10, 60), "World", (0, 0, 0, 255), fonte)
+    img_edit.text((700, 1490), str(x), "#000", fonte)
     img.save(f"{caminho}\{str(x)}.png")
